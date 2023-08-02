d018d3e8e84264958baac566298e36c9749b751a
hoda <h.ryosuke820@gmail.com>
2023-08-02 23:05:08 +0900

    ✨ ヘッダー、フッター作成

---
+++ src/components/organisms/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+export const Footer = () => {
+  return (
+    <footer className="h-10 flex items-center justify-center text-xs">
+      <p>
+        <small>&copy; 2023 Flash CODE</small>
+      </p>
+    </footer>
+  );
+};
---
+++ src/components/organisms/Header/Header.tsx
@@ -0,0 +1,16 @@
+export const Header = () => {
+  return (
+    <header className="h-14 flex items-center">
+      <div className="flex justify-between px-5">
+        <div>Flash Code</div>
+        {/* <nav>
+          <ul>
+            <li>JavaScript</li>
+            <li>テスト</li>
+            <li>テスト</li>
+          </ul>
+        </nav> */}
+      </div>
+    </header>
+  );
+};
