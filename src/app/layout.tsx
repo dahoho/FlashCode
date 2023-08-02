057ebf187277e522c500004dc9e2a2ba1b0a4b38
hoda <h.ryosuke820@gmail.com>
2023-08-02 23:06:27 +0900

    ✨ レイアウト追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Providers } from "./providers";
+import { Header } from "@/components/organisms/Header";
+import { Footer } from "@/components/organisms/Footer";
 
 export const metadata: Metadata = {
   title: "FlashCode",
@@ -11,7 +13,13 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="ja">
       <body>
-        <Providers>{children}</Providers>
+        <Providers>
+          <div className="grid grid-rows-[auto,1fr,auto] grid-cols-1 min-h-screen">
+            <Header />
+            <main className="bg-lightGray">{children}</main>
+            <Footer />
+          </div>
+        </Providers>
       </body>
     </html>
   );
