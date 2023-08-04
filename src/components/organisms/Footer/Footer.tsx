950ba6e0c06868cc144d7f8e0a74341c8291dd3d
hoda <h.ryosuke820@gmail.com>
2023-08-04 22:03:20 +0900

    ✨ コピーライトyear取得

--- src/components/organisms/Footer/Footer.tsx
+++ src/components/organisms/Footer/Footer.tsx
@@ -1,10 +1,11 @@
+import { year } from "@/utils/date";
 import React from "react";
 
 export const Footer: React.FC = () => {
   return (
     <footer className="h-10 flex items-center justify-center text-xs">
       <p>
-        <small>&copy; 2023 Flash CODE</small>
+        <small>&copy; {`${year} Flash Code`}</small>
       </p>
     </footer>
   );
