d42c7735a054397d3fb3532d6c6cbd327f620b51
hoda <h.ryosuke820@gmail.com>
2023-08-04 22:21:11 +0900

    🚧 JavaScriptページ作成途中

---
+++ src/app/lang/javascript/category/core/string/page.tsx
@@ -0,0 +1,12 @@
+import { Container } from "@/components/organisms/Container";
+import React from "react";
+
+const String = () => {
+  return (
+    <Container>
+      <div>String</div>
+    </Container>
+  );
+};
+
+export default String;
